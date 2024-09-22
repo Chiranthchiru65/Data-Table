f6e313dba11ac3f8beb276691ba88c3594969a11
Chiranthchiru65 <chiranthchiru65@gmail.com>
2024-09-22 22:36:34 +0530

    before working on searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,54 +3,7 @@ import DataTable from "./DataTable";
 function App() {
   return (
     <>
-      <div className="p5 h-screen bg-gray-300 p-4 ">
-        <h1 className="text-xl mb-2">Data Table</h1>
-        <DataTable />
-        <table className="w-full border-4 border-gray-700 ">
-          <thead className="bg-gray-200 border-b-4 border-gray-700">
-            <tr>
-              <th className=" p-3 text-sm tracking-wide text-left">No.</th>
-              <th className=" p-3 text-sm tracking-wide text-left">
-                First Name
-              </th>
-              <th className=" p-3 text-sm tracking-wide text-left">
-                Last Name
-              </th>
-              <th className=" p-3 text-sm tracking-wide text-left">Age</th>
-              <th className=" p-3 text-sm tracking-wide text-left">Sex</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="bg-white">
-              <td className="p-3 text-sm font-bold text-blue-500">1</td>
-              <td className="p-3 text-sm text-gray-700">chiranth</td>
-              <td className="p-3 text-sm text-gray-700">
-                Nataraj hira kj jllajsdfj lkjadf jlkjdfj jlkdsjf kjdflja dj
-              </td>
-              <td className="p-3 text-sm text-gray-700">25</td>
-              <td className="p-3 text-sm text-gray-700">M</td>
-            </tr>
-            <tr>
-              <td className="p-3 text-sm font-bold text-blue-500">1</td>
-              <td className="p-3 text-sm text-gray-700">chiranth</td>
-              <td className="p-3 text-sm text-gray-700">
-                Nataraj hira kj jllajsdfj lkjadf jlkjdfj jlkdsjf kjdflja dj
-              </td>
-              <td className="p-3 text-sm text-gray-700">25</td>
-              <td className="p-3 text-sm text-gray-700">M</td>
-            </tr>
-            <tr className="bg-white">
-              <td className="p-3 text-sm font-bold text-blue-500">1</td>
-              <td className="p-3 text-sm text-gray-700">chiranth</td>
-              <td className="p-3 text-sm text-gray-700">
-                Nataraj hira kj jllajsdfj lkjadf jlkjdfj jlkdsjf kjdflja dj
-              </td>
-              <td className="p-3 text-sm text-gray-700">25</td>
-              <td className="p-3 text-sm text-gray-700">M</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <DataTable />
     </>
   );
 }
--- src/DataTable.jsx
+++ src/DataTable.jsx
@@ -1,21 +1,91 @@
-import { useEffect } from "react";
-// import axios from "axios";
+import { useEffect, useState } from "react";
 import { fetcherWithAxios } from "./FetcherWithAxios";
-
+// for alternate rows 1 = white another bg-gray-50
 function DataTable() {
+  const [users, setUsers] = useState([]);
   useEffect(() => {
     const fetchUsers = async () => {
-      //   const rawData = await fetch("https://dummyjson.com/users");
-      //   const parsedData = await rawData.json();
-      //   console.log(parsedData);
-
-      const userData = await fetcherWithAxios("https://dummyjson.com/users");
-      console.log(userData.users);
+      try {
+        const userData = await fetcherWithAxios("https://dummyjson.com/users");
+        setUsers(userData.users);
+        //   console.log(users);
+      } catch (err) {
+        console.log(err);
+      }
     };
     fetchUsers();
   }, []);
 
-  return <div></div>;
+  return (
+    <div className="p5 h-full bg-gray200 p-4  ">
+      <h1 className="text-xl mb-2">Data Table</h1>
+      {/* search bar */}
+
+      <div className="overflow-auto rounded-lg shadow-2xl">
+        <table className="w-full  ">
+          <thead className="bg-gray-100 border-b-2 border-gray-400">
+            <tr>
+              <th className="border border-slate-300 w-15 p-3 text-sm tracking-wide text-left">
+                No.
+              </th>
+              <th className="border border-slate-300 w-20 p-3 text-sm tracking-wide text-left">
+                First Name
+              </th>
+              <th className="border border-slate-300 w-20 p-3 text-sm tracking-wide text-left">
+                Last Name
+              </th>
+              <th className="border border-slate-300  p-3 text-sm tracking-wide text-left">
+                Email
+              </th>
+              <th className="border border-slate-300  p-3 text-sm tracking-wide text-left">
+                Phone
+              </th>
+              <th className="border border-slate-300  p-3 text-sm tracking-wide text-left">
+                Birth Date
+              </th>
+
+              <th className="border border-slate-300  p-3 text-sm tracking-wide text-left">
+                Age
+              </th>
+              <th className="border border-slate-300  p-3 text-sm tracking-wide text-left">
+                Gender
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y  divide-gray-200  ">
+            {users.map((user) => (
+              <tr key={crypto.randomUUID()} className="bg-white">
+                <td className="border border-slate-300 p-3 text-sm font-bold text-blue-500 ">
+                  {user.id}
+                </td>
+                <td className="border border-slate-300 p-3 text-sm text-gray-700 whitespace-nowrap">
+                  {user.firstName}
+                </td>
+                <td className="border border-slate-300 p-3 text-sm text-gray-700 whitespace-nowrap">
+                  {user.lastName}
+                </td>
+                <td className="border border-slate-300 p-3 text-sm text-gray-700 whitespace-nowrap">
+                  {user.email}
+                </td>
+                <td className="border border-slate-300 p-3 text-sm text-gray-700 whitespace-nowrap">
+                  {user.phone}
+                </td>
+                <td className="border border-slate-300 p-3 text-sm text-gray-700 whitespace-nowrap">
+                  {user.birthDate}
+                </td>
+                <td className="border border-slate-300 p-3 text-sm text-gray-700 whitespace-nowrap">
+                  {user.age}
+                </td>
+                <td className="border border-slate-300 p-3 text-sm text-gray-700 whitespace-nowrap">
+                  {user.gender}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default DataTable;
